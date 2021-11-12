b39d199e8466b1c4ed68de204f2f4a180fc4433f
amali <amalifernando17@gmail.com>
2021-11-12 01:49:56 +0000

    initial project setup

---
+++ src/main/java/com/bolton/eventposting/EventpostingApplication.java
@@ -0,0 +1,13 @@
+package com.bolton.eventposting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventpostingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventpostingApplication.class, args);
+	}
+
+}
